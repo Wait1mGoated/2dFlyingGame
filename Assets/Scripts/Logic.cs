516af1b986f7498294a9e83a165f580272a77647
Ian Slack <kevinstratvertyoutube>
2024-04-12 11:51:32 -0500

    Added a settings panel

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Logic : MonoBehaviour
+{
+    public UnityEngine.UI.Button a;
+    public UnityEngine.UIElements.PanelSettings b;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (a.onClick != null)
+        {
+            
+        }
+    }
+}
