87a081cd86f1ccf68b7abf69da1b96f692340b47
Ian Slack <kevinstratvertyoutube>
2024-04-30 11:14:41 -0500

    speed

--- Assets/Scripts/Object Movement.cs
+++ Assets/Scripts/Object Movement.cs
@@ -16,9 +16,10 @@ public class ObjectMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float t = Random.Range(0, .005F);
         objectSpeed *= 1.0001f;
-        transform.position = transform.position + (Vector3.down * objectSpeed);
-        transform.position = transform.position + (Vector3.right * objectSpeed);
+        transform.position = transform.position + (Vector3.down * (objectSpeed - t));
+        transform.position = transform.position + (Vector3.right * (objectSpeed - t));
         float rotAmount = degreesPerSec * Time.deltaTime;
         float curRot = transform.localRotation.eulerAngles.z;
         transform.localRotation = Quaternion.Euler(new Vector3(0, 0, curRot + rotAmount));
